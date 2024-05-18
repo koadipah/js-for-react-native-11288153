4a40c4e447b505111c770a9be272353edf791070
Adipah Osei <adipahosei@gmail.com>
2024-05-18 18:25:15 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -18,7 +18,31 @@ function processArray(numbers) {
     return newArray;
 }
 
+
+function formatArrayStrings(strings, numbers){
+
+    if (numbers.length !== strings.length){
+        throw new Error("Both arrays must have the same length")
+    }
+
+    let newArray = [];
+    for(let i = 0; i < strings.length; i++){
+        if (numbers[i] % 2 === 0) {
+            newArray.push(strings[i].toUpperCase()); 
+        } else {
+            newArray.push(strings[i].toLowerCase())
+        }
+    }
+    return newArray;
+}
+
+
+
 //Example to test 
 const numbersArray = [2,3,4,5,6];
 const result = processArray(numbersArray);
 console.log("New array is: ", result)
+
+const stringsArray = ["One", "Two", "Four", "Five", "Seven",];
+const formattedResult = formatArrayStrings(stringsArray, result);
+console.log("Formatted string array: ",formattedResult)
